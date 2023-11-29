3b28b6d22d5d9e691c2e31f2ee1307bcec37ebbe
Se7Go <asqwzx144@gmail.com>
2023-11-29 06:03:27 +0300

    fix: redis config enabled(?)

--- highload/storage-service/src/main/java/ru/itmo/highload/service/config/RedisConfig.java
+++ highload/storage-service/src/main/java/ru/itmo/highload/service/config/RedisConfig.java
@@ -6,8 +6,9 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
-//@Configuration
-//@Profile("redis")
+
+@Configuration
+@Profile("redis")
 public class RedisConfig {
     @Bean
     public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
